f7d4dbc99b2f6bc204544ced7a72c44bc9546e3c
Daryan <trinhdaryan@gmail.com>
2024-06-09 20:23:11 -0700

    Added Poseidon Armor, Added Tick Event Listener
    - Added Poseidon Armor, no textures, no name
    - Added Tick Event
    - Listens for when player has full Poseidon Armor, gives water breathing

--- src/main/java/xyz/gamars/eos/objects/ItemInit.java
+++ src/main/java/xyz/gamars/eos/objects/ItemInit.java
@@ -1,6 +1,8 @@
 package xyz.gamars.eos.objects;
 
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -15,9 +17,19 @@ public class ItemInit {
     public static final DeferredHolder<Item, Item> DEV_ITEM = ITEMS.register("dev_item", () -> new DevItem(new Item.Properties()));
     public static final DeferredHolder<Item, Item> PLUNGER = ITEMS.register("plunger", () -> new Item(new Item.Properties()));
 
+    public static final DeferredHolder<Item, Item> POSEIDON_HELMET = ITEMS.register("poseidon_helmet",
+            () -> new ArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.HELMET, new Item.Properties()));
+    public static final DeferredHolder<Item, Item> POSEIDON_CHESTPLATE = ITEMS.register("poseidon_chestplate",
+            () -> new ArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
+    public static final DeferredHolder<Item, Item> POSEIDON_LEGGINGS = ITEMS.register("poseidon_leggings",
+            () -> new ArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.LEGGINGS, new Item.Properties()));
+    public static final DeferredHolder<Item, Item> POSEIDON_BOOTS = ITEMS.register("poseidon_boots",
+            () -> new ArmorItem(ArmorMaterials.DIAMOND, ArmorItem.Type.BOOTS, new Item.Properties()));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
+
     }
 
 }
--- src/main/java/xyz/gamars/eos/objects/items/DevItem.java
+++ src/main/java/xyz/gamars/eos/objects/items/DevItem.java
@@ -1,17 +1,14 @@
 package xyz.gamars.eos.objects.items;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
 import xyz.gamars.eos.Eos;
 
 public class DevItem extends Item {
