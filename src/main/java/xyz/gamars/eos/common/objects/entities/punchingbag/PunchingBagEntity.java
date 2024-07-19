393db14ba98ef6b22bb64ff9cc9b8c5f5a967fcc
Daryan <trinhdaryan@gmail.com>
2024-07-19 08:58:01 -0700

    Punching Bag now extends Mob

--- src/main/java/xyz/gamars/eos/common/objects/entities/punchingbag/PunchingBagEntity.java
+++ src/main/java/xyz/gamars/eos/common/objects/entities/punchingbag/PunchingBagEntity.java
@@ -2,10 +2,7 @@ package xyz.gamars.eos.common.objects.entities.punchingbag;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.HumanoidArm;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
@@ -24,15 +21,14 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class PunchingBagEntity extends LivingEntity implements GeoEntity {
-
-    protected static final RawAnimation IDLE = RawAnimation.begin().thenLoop("idle");
+public class PunchingBagEntity extends Mob implements GeoEntity {
+    
     protected static final RawAnimation ATTACKED = RawAnimation.begin().thenPlay("attacked");
 
     private final AnimatableInstanceCache animatableInstanceCache = GeckoLibUtil.createInstanceCache(this);
 
 
-    public PunchingBagEntity(EntityType<? extends LivingEntity> entityType, Level level) {
+    public PunchingBagEntity(EntityType<? extends Mob> entityType, Level level) {
         super(entityType, level);
 
         SingletonGeoAnimatable.registerSyncedAnimatable(this);
@@ -41,31 +37,9 @@ public class PunchingBagEntity extends LivingEntity implements GeoEntity {
     }
 
     public static AttributeSupplier.Builder createAttributes() {
-        return LivingEntity.createLivingAttributes()
+        return Mob.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 1024)
                 .add(Attributes.KNOCKBACK_RESISTANCE, 1);
-    }
-
-    @Override
-    public Iterable<ItemStack> getArmorSlots() {
-        ArrayList<ItemStack> armor = new ArrayList<>();
-        armor.add(ItemStack.EMPTY);
-        return armor;
-    }
-
-    @Override
-    public ItemStack getItemBySlot(EquipmentSlot slot) {
-        return ItemStack.EMPTY;
-    }
-
-    @Override
-    public void setItemSlot(EquipmentSlot slot, ItemStack stack) {
-
-    }
-
-    @Override
-    public HumanoidArm getMainArm() {
-        return HumanoidArm.RIGHT;
     }
 
     @Override
